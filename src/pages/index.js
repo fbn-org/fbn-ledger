b1531b0367a91e9ace386ca6d090cc8205fb729b
colin <42125428+colinhartigan@users.noreply.github.com>
2023-12-20 03:30:52 -0500

    fix?

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,9 @@
-import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 
 export default function Printing(props) {
     const router = useRouter();
-    const { data: session } = useSession();
     // Make sure we're in the browser
     if (typeof window !== 'undefined') {
-        if (!session) {
-            router.push('/auth');
-        } else {
-            router.push('/app/dashboard');
-        }
+        router.push('/app/dashboard');
     }
 }
